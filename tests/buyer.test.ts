1c28b073a88e2a2a839deaf85396cad8509c19c0
Denis Aydo <pythonist001@proton.me>
2025-03-15 18:27:02 +0300

    #356 Работа с токеном в смарт-контракте, метод accept_stream_data

---
+++ tests/buyer.test.ts
@@ -0,0 +1,74 @@
+import {
+    getAssociatedTokenAddressSync,
+    createAssociatedTokenAccountInstruction,
+    TOKEN_PROGRAM_ID,
+    TOKEN_2022_PROGRAM_ID
+} from "@solana/spl-token";
+
+describe("Buyer", () => {
+    const provider = anchor.AnchorProvider.env();
+    anchor.setProvider(provider);
+
+    const wallet = provider.wallet as anchor.Wallet;
+    const owner = wallet.publicKey;
+    const programId = pg.program.programId;
+    const mint = new web3.PublicKey("AYMuaTVib2XrPwStrWaVW7k2yeZmDVRc61SFvDjJTwF3");
+    const tokenProgramId = TOKEN_2022_PROGRAM_ID;
+
+    console.log("Owner:", owner.toBase58());
+    console.log(`Using mint: ${mint.toBase58()}`);
+    console.log(`Using token program id: ${tokenProgramId}`);
+
+    it("Create", async () => {
+        const [buyer, bump] = web3.PublicKey.findProgramAddressSync(
+            [Buffer.from("buyer"), owner.toBuffer()],
+            programId
+        );
+
+        console.log(`Buyer: ${buyer}`);
+
+        const buyerTokenAccount = getAssociatedTokenAddressSync(mint, buyer, true, tokenProgramId);
+        console.log("Buyer Token Account:", buyerTokenAccount?.toBase58());
+
+        let buyerTokenAccountInfo = await pg.connection.getAccountInfo(buyerTokenAccount);
+        if (!buyerTokenAccountInfo) {
+            console.log("Creating buyer's token account...");
+
+            const createATAIx = createAssociatedTokenAccountInstruction(
+                owner,
+                buyerTokenAccount,
+                buyer,
+                mint,
+                tokenProgramId
+            );
+
+            const createTx = new web3.Transaction().add(createATAIx);
+            await web3.sendAndConfirmTransaction(pg.connection, createTx, [wallet.payer]);
+
+            buyerTokenAccountInfo = await pg.connection.getAccountInfo(buyerTokenAccount);
+            if (!buyerTokenAccountInfo) {
+                throw new Error("buyerTokenAccount was not created successfully!");
+            }
+        }
+
+        const buyerBalance = await pg.connection.getTokenAccountBalance(buyerTokenAccount);
+        console.log(`Buyer Token Balance: ${buyerBalance.value.amount}`);
+
+        const txHash = await pg.program.methods
+            .createBuyer()
+            .accounts({
+                buyer: buyer,
+                owner: owner,
+                systemProgram: web3.SystemProgram.programId,
+            })
+            .rpc();
+        console.log(`Transaction sent: ${txHash}`);
+
+        const buyerData = await pg.program.account.buyer.fetch(
+            buyer
+        );
+
+        console.log(`Buyer Data:`);
+        console.log(buyerData);
+    });
+});
